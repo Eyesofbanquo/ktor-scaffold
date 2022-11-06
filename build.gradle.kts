e9b75200f47cb9f9a7cb0fe05a9076434dc83c19
Markim Shaw <markim.shaw@e-hps.com>
2022-11-05 22:05:12 -0500

    base proj

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.20"
     application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+val ktor_version = "2.1.3"
+val kotlin_version = "1.7.20"
 
 repositories {
     mavenCentral()
@@ -14,13 +17,22 @@ repositories {
 
 dependencies {
     /* Ktor */
-    implementation("io.ktor:ktor-server-core:2.1.3")
+    implementation("io.ktor:ktor-server-core:2.1.3") {
+        because("this is for the Ktor core ")
+    }
     implementation("io.ktor:ktor-server-netty:2.1.3")
 
-    implementation("ch.qos.logback:logback-classic:1.4.4")
+    implementation("ch.qos.logback:logback-classic:1.4.4") {
+        because("This is to remove log error for scaffold")
+    }
+
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
 
     /* Test dependencies */
     testImplementation(kotlin("test"))
+    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
 
 tasks.test {
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,20 +1,22 @@
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-    }.start(wait = true)
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+}
+fun Application.configureRouting() {
+    routing {
+    }
 }
 
-fun Application.configureRouting() {
-
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
     }
 }
